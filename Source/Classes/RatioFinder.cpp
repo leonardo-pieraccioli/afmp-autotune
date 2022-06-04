f47645f60ce394d38de7df313985e929a8f7a9cf
qmEzequiel <tabassocarlo@gmail.com>
2022-06-04 12:38:17 +0200

    Branch prova

--- Source/Classes/RatioFinder.cpp
+++ Source/Classes/RatioFinder.cpp
@@ -1,11 +1,30 @@
 //includes on top
 #include "RatioFinder.hpp"
 
+
 //constructor
 RatioFinder::RatioFinder(){}
 
 //implementation of functions in autotune.m
 float RatioFinder::getRatio(const float* buffer_channel_pointer){
-
+    float* mag_buffer = const_cast<float*> (buffer_channel_pointer);
     
+    //computing FFT (not sure about type conversion and compatibility)
+    //std::vector<float> buffer_in_frequency  = FFT( (std::vector<float>)* buffer_channel_pointer);
+    juce::dsp::FFT FFT(10);
+    FFT.performFrequencyOnlyForwardTransform(mag_buffer);
+
+    return near_f/peak_f;
 }
+
+//FFT
+//std::vector<float> RatioFinder::FFT(std::vector<float> buffer_in_time){}
+
+//find nearest note
+void RatioFinder::nearestNote(){
+    near_f = peak_f;
+}
+
+//index to frequency and viceversa converters
+//int RatioFinder::freqToIndx(const float* f){}
+//float RatioFinder::indxToFreq(const int* i){}
--- Source/Classes/RatioFinder.hpp
+++ Source/Classes/RatioFinder.hpp
@@ -1,11 +1,22 @@
 #ifndef RATIO_FINDER_H
 #define RATIO_FINDER_H
 
+#include <vector>
+#include <juce_dsp/juce_dsp.h>
+
+
 class RatioFinder {
     private:
         //pricate variables
-        
+        float peak_f;
+        float near_f;
         //private methods
+        //std::vector<float> FFT(std::vector<float> buffer_in_time);
+        
+        void nearestNote();
+
+        int freqToIndx(const float* f);
+        float indxToFreq(const int* i);
 
     public: 
         //constructor

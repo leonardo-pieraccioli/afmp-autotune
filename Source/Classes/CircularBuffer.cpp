a97bf2f9a1f69ced26affc1f022eaae59e8edcfc
qmEzequiel <tabassocarlo@gmail.com>
2022-06-14 16:37:23 +0200

    First try on the CircularBuffer, surely to improve, not even sure it works, but i commit it to save it

--- Source/Classes/CircularBuffer.cpp
+++ Source/Classes/CircularBuffer.cpp
@@ -1,33 +1,75 @@
-//includes on top
 #include "CircularBuffer.hpp"
-
 //constructor
-CircularBuffer::CircularBuffer(){
-    for(int i = 0; i<size; i++){
-        buffer.push_back(0);
-    }
+CircularBuffer::CircularBuffer(int s){
+    size = s;
+    end_i = size;
+    window = std::vector<float>(size,0.0);
 }
 
-//main method 
-void CircularBuffer::in_out (std::vector<float> inout){
-    end_i += inout.size();
+//overload operator []
+float &CircularBuffer::operator[](int i){
+    int a = i/size;  //ratio rounded in case i is few times higher than size
+    int j = i + start_read_i; //set index 0 to start_read_i
+    if( j < 0){
+        if( j <= -size){
+            return window [j + size*(a+1)];
+        }
+        return window [j + size];
+    }
+    if( j >= size){
+        return window [j - size*a];
+    }
+    return window[j];
+}
+
+//flag
+bool CircularBuffer::will_be_full(const int& buffer_size){
+    if(start_read_i + buffer_size > end_i){
+        return true;
+    }
+    return false;
+}
+
+//read and write of the buffer
+std::vector<float> CircularBuffer::buffer_read_and_write(const std::vector<float>& bufferIN){
+    std::vector<float> bufferOUT;
+    int buffer_size = bufferIN.size();
+
+    //sending output
+    for (int i = 0; i<buffer_size; i++){
+        bufferOUT.push_back((*this)[i]);
+    }  
+
+    //memorizing input
+    for (int i = 0; i<buffer_size; i++){
+        (*this)[i] = bufferIN[i];
+    }
+
+    //resetting start index, if it surpass the size of the vector i subtract the size form it to keep it from going up to infinity
+    int temp = start_read_i + buffer_size;
+    if(temp >= size) {
+        start_read_i = temp - size;
+    } else start_read_i = temp;
+}
+
+
+//sending window to elaborare
+std::vector<float> CircularBuffer::get_window_to_elaborate(){
+    std::vector<float> temp_window;
     
-    std::vector<float> temp_in;
-    //copying
-    for(int i=0; i<inout.size(); i++){
-        temp_in[i] = inout[i];
+    for (int i = end_i + 1; i< start_read_i; i++){
+        temp_window.push_back((*this)[i]);
     }
-    //sending the output
-    for(int i=0; i<inout.size(); i++){
-        inout[i] = buffer[start_i + i];
-    }
-    //memorizing the input
-    for(int i=0; i<inout.size(); i++){
-        buffer[start_i + i] = temp_in[i];
-    }
+
+    return temp_window; 
 }
 
-//output method
-std::vector<float>* CircularBuffer::get_buffer(){
-    return &buffer;
+//receiving elaborated window
+void CircularBuffer::set_window_once_elaborate(const std::vector<float>& w){
+    
+    for (int i = end_i + 1, j=0; i< start_read_i, j<w.size(); i++, j++){
+        (*this)[i]=w[j];
+    }
+
+    end_i = start_read_i-1;
 }
--- Source/Classes/CircularBuffer.hpp
+++ Source/Classes/CircularBuffer.hpp
@@ -2,29 +2,81 @@
 #define CIRCULAR_BUFFER_H
 
 #include <vector>
-#include <algorithm>
+#include <iostream>
 
 class CircularBuffer {
     private:
-        //pricate variables
-        const static int size = 10;
-        std::vector<float> buffer;
-        int start_i = 0;
-        int end_i = 0;
-        bool is_full_flag = false;
-        
-        //private methods
+    int size = 0; 
+    int start_read_i = 0;
+    int end_i = 0; //end of the array, end_i+1 is the start of the computed part
 
-    public: 
-        //constructor
-        CircularBuffer();
-
-        //public variables
-
-        //public methods
-        void in_out (std::vector<float> inout);
-
-        std::vector<float>* get_buffer();
+    public:
+    std::vector<float> window;
+    //constructor
+    CircularBuffer(int s);
+    //overload operator
+    float &operator[](int i);
+    //flag
+    bool will_be_full(const int& buffer_size);
+    //elaboration
+    std::vector<float> get_window_to_elaborate();
+    void set_window_once_elaborate(const std::vector<float>& w);
+    //inout
+    std::vector<float> buffer_read_and_write( const std::vector<float>& bufferIN);
 };
 
-#endif //RATIO_FINDER_H
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class CircularBuffer {
+//     private:
+//         //pricate variables
+//         const static int size = 10;
+//         std::vector<float> window;
+//         std::vector<float> memory;
+//         int start_i = 0;
+//         int end_i = 0;
+//         bool is_full_flag = false;
+//         int offset=0;
+        
+//         //private methods
+
+//     public: 
+//         //constructor
+//         CircularBuffer();
+
+//         //public variables
+
+//         //public methods
+//         void in_out (std::vector<float>& inout);
+
+//         //float &operator[](int i);
+
+//         std::vector<float> get_window();
+//         void set_window(std::vector<float> buffer);
+//         bool get_flag ();
+// };
+
+#endif //CIRCULAR_BUFFER_H

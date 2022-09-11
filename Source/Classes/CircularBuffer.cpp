3845ae02b4442b6a8a4ae7799b4c57db844c15a8
Leonardo Pieraccioli <leonardo.pieraccioli@gmail.com>
2022-09-12 01:27:26 +0200

    Corrected start-end in CircularBuffer.cpp

--- Source/Classes/CircularBuffer.cpp
+++ Source/Classes/CircularBuffer.cpp
@@ -3,7 +3,7 @@
 CircularBuffer::CircularBuffer(int s){
     size = s;
     end_i = size-1;
-    window = std::vector<float>(size,0.0);
+    window = std::vector<float>(size,0);
 }
 
 //overload operator [] for the class, it modify the window variable
@@ -24,10 +24,18 @@ float &CircularBuffer::operator[](int i){
 
 //flag function used as a condition for the elaboration part
 bool CircularBuffer::will_be_full(const int& buffer_size){
-    if(start_i + buffer_size > end_i){
-        return true;
+    if (start_i < end_i) {
+        if (start_i + buffer_size > end_i) {
+            return true;
+        }
+        return false;
     }
-    return false;
+    else {
+        if (start_i + buffer_size > size + end_i + 1) { // size = 10, start = 8, buf = 4, end = 1 -> start + buf = 12, size + end + 1 = 12
+            return true;
+        }
+        return false;
+    }
 }
 
 //read and write of the buffer given by JUCE
@@ -38,7 +46,7 @@ std::vector<float> CircularBuffer::buffer_read_and_write(const std::vector<float
     //sending output
     for (int i = 0; i<buffer_size; i++){
         bufferOUT.push_back((*this)[i]);
-    }  
+    }
 
     //memorizing input
     for (int i = 0; i<buffer_size; i++){
@@ -46,19 +54,25 @@ std::vector<float> CircularBuffer::buffer_read_and_write(const std::vector<float
     }
 
     //resetting start index, if it surpass the size of the vector i subtract the size form it to keep it from going up to infinity
-    int temp = start_i + buffer_size;
+    int temp = start_i + buffer_size; // size = 10, start = 8, buf_size = 3; -> temp = 11
     if(temp >= size) {
-        start_i = temp - size;
+        start_i = temp - size; // start = 1 -> 11 - 10 = 1 ok
     } else start_i = temp;
 
     return bufferOUT;
 }
 
 
-//sending window to elaborate 
+//sending window to elaborate
 std::vector<float> CircularBuffer::get_window_to_elaborate(){
-    int a = size+start_i-end_i-1;
+    int a;
+    if (start_i > end_i)
+        a = start_i - end_i - 1; // size = 10, start = 8, end = 2 -> a = 5;
+    else
+        a = size - (end_i - start_i) - 1 ; // size = 10, start = 2, end = 8 -> a = 3
+
     std::vector<float> temp_window(a,0);
+
     for (int i = 1; i<=a; i++){
         temp_window[a-i] = (*this)[-i];
     }
@@ -68,8 +82,12 @@ std::vector<float> CircularBuffer::get_window_to_elaborate(){
 
 //receiving elaborated window
 void CircularBuffer::set_window_once_elaborate(const std::vector<float>& w){
-    int a = size+start_i-end_i-1;
-    
+    int a;
+    if (start_i > end_i)
+        a = start_i - end_i - 1; // size = 10, start = 8, end = 2 -> a = 5;
+    else
+        a = size - (end_i - start_i) - 1 ; // size = 10, start = 2, end = 8 -> a = 3
+
     for (int i = 1; i<=a; i++){
         (*this)[-i] = w[a-i];
     }
